0161f48af5f9a3157d76a6a23e692cacc9ebfc21
tiwarimanikanand007 <tiwarimanikanand@gmail.com>
2026-09-13 23:57:48 +0530

    Update tt_um_cpu_wrapper.v

--- src/tt_um_cpu_wrapper.v
+++ src/tt_um_cpu_wrapper.v
@@ -54,7 +54,7 @@
  * ============================================================================
  */
 
-module tt_um_cpu_wrapper (
+module tt_um_tiwarimanikanand007_cpu (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: input path
